3dc0b38d3c816938d77a9d86832eb4fcf0166372
EstebanGutierrez <egutierrezc93@gmail.com>
2023-08-30 11:31:10 -0500

    first challenge endpoint implemented, filtering sensor data

--- api/service/sensor_data_service.py
+++ api/service/sensor_data_service.py
@@ -1,5 +1,6 @@
-from flask import Request, Response
+from flask import Request, Response, jsonify
 import pandas as pd
+import json
 
 class SensorDataService():
 
@@ -21,19 +22,26 @@ class SensorDataService():
         return filtered_df
     
     def __filter_sensor_data__(self, df:pd.DataFrame, sensor:str, min:int=20, max:int=30) -> pd.DataFrame:
-        filtered_df:pd.DataFrame = df[['date', 'time', sensor]]
+        filtered_df:pd.DataFrame = df[['date', 'time', sensor, 'machine_status']]
         filtered_df = filtered_df.loc[filtered_df[sensor] < max]
         filtered_df = filtered_df.loc[filtered_df[sensor] > min]
+        filtered_df = filtered_df.rename({sensor: 'measure'}, axis='columns')
+        filtered_df['sensor'] = sensor
         return filtered_df
+    
+    def __get_filtered_sensors_data__(self, df:pd.DataFrame) -> dict:
+        sensor_07_filtered_df:pd.DataFrame = self.__filter_sensor_data__(df, sensor='sensor_07')
+        sensor_47_filtered_df:pd.DataFrame = self.__filter_sensor_data__(df, sensor='sensor_47')
+        filtered_sensor_data_df:pd.DataFrame = pd.concat([sensor_07_filtered_df, sensor_47_filtered_df])
+        return filtered_sensor_data_df.to_dict('records')
 
     def get_filtered_sensor_data(self) -> Response:
         try:
             sensor_df:pd.DataFrame = self.__read_data_from_csv__()
             date_filtered_df:pd.DataFrame = self.__filter_by_date__(sensor_df)
-            sensor_07_filtered_df:pd.DataFrame = self.__filter_sensor_data__(date_filtered_df, sensor='sensor_07')
-            sensor_47_filtered_df:pd.DataFrame = self.__filter_sensor_data__(date_filtered_df, sensor='sensor_47')
-            print('sensor_07_filtered_df ->', sensor_07_filtered_df.values.tolist())
-            print('sensor_47_filtered_df ->', sensor_47_filtered_df)
-            return {"message": "ok"}, 200
-        except:
+            filtered_sensor_data_dict:dict = self.__get_filtered_sensors_data__(date_filtered_df)
+            response:Response = jsonify(filtered_sensor_data_dict), 200
+            return response
+        except Exception as error:
+            print('An exception ocurred in SensorDataService.get_filtered_sensor_data', error)
             return {"message": "Internal Server Error"}, 500
